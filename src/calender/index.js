7479f7f57097256c6945670e46d77366d61c1bf6
kewon96 <yang0184@naver.com>
2020-04-15 23:23:48 +0900

    유동적으로 Element가 생성되게 변경

--- src/calender/index.js
+++ src/calender/index.js
@@ -1,57 +1,91 @@
 function Main() {
-    const day = document.getElementsByClassName('day');
-    // const createDay = document.createElement('p');
-    const before = document.getElementsByClassName('before');
-    
+    // 주 생성
+    for (let i = 0; i < 6; i++) {
+        // create
+        const weeks = document.createElement('div');
+
+        // class값 넣기
+        weeks.setAttribute('class', 'weeks');
+
+        document.getElementsByClassName('month')[0].append(weeks);
+    }
+
     // 저번달
-    if(firstDayWeek() !== 0) {
-        const lastDay = beforeMonthLast();
-        for (let i = 0; i < firstDayWeek(); i++) {
-            const element = before[i];
-            
-            element.innerHTML = i;
+    const firstDay = dayOfWeek(firstDayWeek());
+
+    if(firstDay !== 0) {
+        let lastDay = beforeMonthLast() - firstDay + 1; // 달력에 보여질 저번 달의 날짜 중 제일 낮은 날짜
+        for (let i = 0; i < firstDay; i++) {
+            // create
+            const beforeElement = document.createElement('p');
+            beforeElement.appendChild(document.createTextNode(lastDay++));
+
+            // class값 넣기
+            beforeElement.setAttribute('class', 'before day');
+
+            // 첫번째 weeks에 넣기(document.getElementsByClassName('weeks')[0])
+            document.getElementsByClassName('weeks')[0].append(beforeElement);
         }
     }
     
-    let temp = 1;
     // 이번달
-    for (let i = firstDayWeek(); i < lastDay() + firstDayWeek(); i++) {
-        const element = day[i];
+    let order = 0; // 주 순서
+    for (let i = 1; i <= lastDay(); i++) {
+        // create
+        const nowElement = document.createElement('p');
+        nowElement.appendChild(document.createTextNode(i));
+        
+        // class값 넣기
+        nowElement.setAttribute('class', 'now day')
 
-        element.innerHTML = temp;
-        temp++;
+        // 순차적으로 넣기(document.getElementsByClassName('weeks')[order])
+        let weeks = document.getElementsByClassName('weeks')[order];
+        weeks.append(nowElement);
+
+        if(weeks.childElementCount >= 7) {
+            order++;
+        } 
     }
 
     // 다음달
+    /**
+     * 현재 달의 마지막날의 요일이 토요일이라면 6번째줄에만 보여지게
+     * 그게 아니라면 5번째줄부터
+     */
+    for (let i = 1; i <= lastDay(); i++) {
+        // create
+        const afterElement = document.createElement('p');
+        afterElement.appendChild(document.createTextNode(i));
+        
+        // class값 넣기
+        afterElement.setAttribute('class', 'after day');
 
+        let weeks = document.getElementsByClassName('weeks')[order];
+        weeks.append(afterElement);
+
+        if(weeks.childElementCount >= 7) {
+            order++;
+        } 
+    }
 
 }
 
-// 오늘 요일 return(0 ~ 6 => 일요일 ~ 토요일)
-function dayOfWeek() {
-    const weeks = new Date().getDay();
+// 요일 return(0 ~ 6 => 일요일 ~ 토요일)
+const dayOfWeek = (param) => {
+    return param.getDay();
+} 
 
-    return weeks;
+// 이번 달의 첫날
+const firstDayWeek = () => {
+    return new Date(new Date().getFullYear(), new Date().getMonth(), 1);
 }
 
-// 이번 달의 마지막날 return
-function lastDay() {
-    const now = new Date(); // 월 count가 0부터 시작
-    const last = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-    
-    return last;
-}
-
-// 이번달의 첫날 요일 추출
-function firstDayWeek() {
-    const now = new Date();
-
-    const firstDate = new Date(now.getFullYear(), now.getMonth(), 1);
-    
-    return firstDate.getDay();
+// 이번 달의 마지막날
+const lastDay = () => {
+    return new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
 }
 
 // 저번달의 마지막날
-function beforeMonthLast() {
+const beforeMonthLast = () => {
     return new Date(new Date().getFullYear(), 0, 0).getDate(); // 올해 1월일 경우 전년도 12월 31일로 나타남
 }
